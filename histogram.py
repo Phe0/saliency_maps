5f4aead558221a54a15db734ea2d8d4f346749f0
Sara Silva <saracsas2@gmail.com>
2019-12-20 08:50:47 -0300

    Histogram normalization and changes in histograms file

--- histogram.py
+++ histogram.py
@@ -1,29 +1,42 @@
 import cv2
 import os
 
-def para_cinza(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+header = """@relation BaseImagens
+@attribute h0 NUMERIC
+@attribute h1 NUMERIC
+....
+@atttibute h255 NUMERIC
+@attribute classe STRING
+@data\n
+"""
+
+def get_gray_scale(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # get gray scale of the image
     return gray
 
-def extrair_caracteristica(img):
-    hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+def get_histogram(img):
+    hist = cv2.calcHist([img], [0], None, [256], [0, 256]) # get histogram
 
+    cv2.normalize(hist, hist) # normalizes the histogram
     histString = ''
 
     for i in hist:
-        histString = histString + str(i[0]) + ' '
+        histString = histString + str(i[0]) + ', ' # get histogram string
     return histString
 
-if __name__ == '__main__':
-    path = './MedDB5000/'
-
-    with open('histograma.txt', 'w') as text:
-        for d, _, fs in os.walk(path):
+def write_histograms_file(path): # write file with histograms
+    with open('histograms.arff', 'w') as text:
+        text.write(header)
+        for d, _, fs in os.walk(path): 
             for f in fs:
                 imgPath = d + '/' + f
                 img = cv2.imread(imgPath)
 
                 if img is not None:
-                    img = para_cinza(img)
-                    hist = extrair_caracteristica(img) + ' ' + d.split('/')[2] + '\n'
+                    img = get_gray_scale(img)
+                    hist = get_histogram(img) + d.split('/')[2] + '\n'
                     text.write(hist)
+
+path = './MedDB5000/'
+write_histograms_file(path)
+
