e66a9eb56dda7036fbeaa7d413b76abfab6081c7
phe0 <pheofo@gmail.com>
2019-12-21 15:13:39 -0300

    add lbp

--- histogram.py
+++ histogram.py
@@ -1,16 +1,18 @@
+from skimage import feature
+import numpy as np
 import cv2
 import os
 
-def make_header():
+def make_header(rangeSize):
     header = "@relation BaseImagens"
 
-    for i in range(256):
+    for i in range(rangeSize):
         header += f"\n@attribute h{i} NUMERIC"
 
     header += "\n@attribute classe {"
 
-    for i in range(1, 31):
-        header += "{i},"
+    for i in range(1, 32):
+        header += f"{i},"
     
     header+= "32}\n@data\n\n"
     
@@ -21,18 +23,27 @@ def get_gray_scale(img):
     return gray
 
 def get_histogram(img):
-    hist = cv2.calcHist([img], [0], None, [256], [0, 256]) # get histogram
+    print(img.dtype)
+    print(img)
+    hist = cv2.calcHist([img], [0], None, [27], [0, 26]) # get histogram
 
     cv2.normalize(hist, hist) # normalizes the histogram
     histString = ''
 
     for i in hist:
         histString = histString + str(i[0]) + ',' # get histogram string
+    print(histString)
     return histString
+
+def get_LBP(img, numPoints, radius):
+    lbp = feature.local_binary_pattern(img, numPoints, radius, method="uniform")
+    lbp = np.float32(lbp)
+    return lbp
+
 
 def write_histograms_file(path): # write file with histograms
     with open('histograms.arff', 'w') as text:
-        text.write(make_header())
+        text.write(make_header(27))
         i = 0
         for d, _, fs in os.walk(path): 
             for f in fs:
@@ -41,10 +52,12 @@ def write_histograms_file(path): # write file with histograms
 
                 if img is not None:
                     img = get_gray_scale(img)
+                    img = get_LBP(img, 24, 8)
                     hist = get_histogram(img) + ' ' + str(i) + '\n'
                     text.write(hist)
             i+=1
 
 path = './MedDB5000/'
+
 write_histograms_file(path)
 
